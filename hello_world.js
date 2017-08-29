47828a9b210bfb65505e18313c3a6c61cd859a19
Tyler <tzptrans@gmail.com>
2017-08-29 14:39:06 -0400

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -1,2 +1,12 @@
-//Hello World in Javascript
-alert("Hello, World!");
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+function hw(){
+alert(“Hello, World!”) //Hello World in Javascript
+}
+</script>
+</head>
+<body>
+</body>
+</html>
